3de531e3453b4500d708875a2da8a3e3d5bc666b
Edward <edwardr1213@gmail.com>
2026-09-12 12:23:15 -0500

    refactor(api): remove placeholder Application record and hardcoded routes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
@@ -6,19 +8,6 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-var applications = new List<Application> { new(1, "Company A", "Developer", "Applied")};
-
-app.MapGet("/applications", () => applications);
-
-app.MapPost("/applications", (Application application) =>
-{
-    applications.Add(application);
-    return Results.Created($"/applications/{application.Id}", application);
-}); 
-
 app.Run();
-
-internal record Application(int Id, string Company, string Role, string Status);
-
 
 
