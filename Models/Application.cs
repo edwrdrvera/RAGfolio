5eaa06127527c24d40a8638bd3dcf1740db674d3
Edward <edwardr1213@gmail.com>
2026-09-12 12:22:02 -0500

    feat(models): add Company and Application entities with Status enum

--- Models/Application.cs
+++ Models/Application.cs
@@ -1,10 +1,14 @@
 namespace JobLedger.Models;
 
+public enum Status { Applied, Interviewing, Rejected, Offer };
 public class Application
 {
     public int Id { get; set; }
     public required int CompanyId { get; set; }
     public required Company Company { get; set; }
     public required string Role { get; set; }
-    public required string Status { get; set; }
+
+    // Creates an enum property Status
+    public required Status Status { get; set; } = Status.Applied;
+    
 }
--- Models/Company.cs
+++ Models/Company.cs
@@ -4,4 +4,7 @@ public class Company
 {
     public int Id { get; set; }
     public required string Name { get; set; }
+
+    // Form a bi-directional relationship with Applications so that Company has a list of corresponding Applications
+    public List<Application> Applications { get; set; } = []; 
 }
